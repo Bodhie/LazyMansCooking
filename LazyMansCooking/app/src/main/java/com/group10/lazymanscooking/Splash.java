8f5879eb603810efac8554e476d5d15fdef7da87
Bodhie <stefanbode1995@hotmail.com>
2015-12-16 18:15:27 +0100

    Splash screen

---
+++ LazyMansCooking/app/src/main/java/com/group10/lazymanscooking/Splash.java
@@ -0,0 +1,26 @@
+package com.group10.lazymanscooking;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class Splash extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        ImageView splashImage = (ImageView) findViewById(R.id.splashImage);
+        splashImage.setImageResource(R.drawable.lazymanscooking);
+
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(Splash.this, MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 4000);
+    }
+}
