e21e640922f4b2a5e269b8ce740be3baae73ccef
Bodhie <stefanbode1995@hotmail.com>
2016-01-12 11:36:20 +0100

    Add recipe and fixes

--- LazyMansCooking/app/src/main/java/com/group10/lazymanscooking/addRecipeFragment.java
+++ LazyMansCooking/app/src/main/java/com/group10/lazymanscooking/addRecipeFragment.java
@@ -2,14 +2,20 @@ package com.group10.lazymanscooking;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseObject;
 
 /**
  * Created by Stefan on 2016-01-08.
  */
-public class addRecipeFragment extends Fragment {
+public class addRecipeFragment extends Fragment implements View.OnClickListener {
     View rootView;
 
     @Override
@@ -17,6 +23,36 @@ public class addRecipeFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         rootView = inflater.inflate(R.layout.activity_add_recipe, container, false);
 
+        Button b = (Button) rootView.findViewById(R.id.btnAddRecipe);
+        b.setOnClickListener(this);
+
         return rootView;
+    }
+
+    public void addRecipe(View v){
+        EditText viewTitle = (EditText) rootView.findViewById(R.id.txtTitle);
+        String title = viewTitle.getText().toString();
+
+        ParseObject recipe = new ParseObject("Recipe");
+        recipe.put("title", title);
+        //TODO : Second value
+        //gameScore.put("playerName", "Sean Plott");/
+        recipe.saveInBackground();
+
+        //Change fragment
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.pager, new  RecipesFragment());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnAddRecipe:
+                addRecipe(v);
+                break;
+        }
     }
 }
