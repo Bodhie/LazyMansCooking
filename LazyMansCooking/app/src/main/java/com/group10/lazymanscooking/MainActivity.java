a3c42a254cc8e1acdd47656430bad21b432dd324
Bodhie <stefanbode1995@hotmail.com>
2016-01-13 14:24:01 +0100

    Search fix

--- LazyMansCooking/app/src/main/java/com/group10/lazymanscooking/MainActivity.java
+++ LazyMansCooking/app/src/main/java/com/group10/lazymanscooking/MainActivity.java
@@ -11,8 +11,15 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity
@@ -43,6 +50,26 @@ public class MainActivity extends AppCompatActivity
         RecipesFragment myFragment = new RecipesFragment();
         fragmentTransaction.add(R.id.pager, myFragment);
         fragmentTransaction.commit();
+
+        //Enter handler
+        final EditText edittext = (EditText) findViewById(R.id.txtSearch);
+        edittext.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    RecipesFragment f = new RecipesFragment();
+                    Bundle args = new Bundle();
+                    args.putString("search", String.valueOf(edittext.getText()));
+                    f.setArguments(args);
+                    FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                    transaction.replace(R.id.pager, f);
+                    transaction.addToBackStack(null);
+                    transaction.commit();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
@@ -71,16 +98,28 @@ public class MainActivity extends AppCompatActivity
         Fragment newFragment = new RecipesFragment();
         
         if (id == R.id.action_search) {
-            //viewPager.setCurrentItem(viewPager.getCurrentItem());
+            TextView search = (TextView) findViewById(R.id.txtSearch);
+            String ssearch = search.getText().toString();
+            System.out.println(ssearch);
+            RecipesFragment f = new RecipesFragment();
+            Bundle args = new Bundle();
+            args.putString("search", ssearch);
+            f.setArguments(args);
+            newFragment = f;
+            System.out.println("search");
         }
         else if(id == R.id.action_advancedSearch)
         {
            //viewPager.setCurrentItem(5);
         }
+        // Create new transaction
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.pager, newFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
 
         return super.onOptionsItemSelected(item);
     }
-
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -111,13 +150,8 @@ public class MainActivity extends AppCompatActivity
 
         // Create new transaction
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
-
-        // Replace whatever is in the fragment_container view with this fragment,
-        // and add the transaction to the back stack
         transaction.replace(R.id.pager, newFragment);
         transaction.addToBackStack(null);
-
-        // Commit the transaction
         transaction.commit();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
--- LazyMansCooking/app/src/main/java/com/group10/lazymanscooking/RecipesFragment.java
+++ LazyMansCooking/app/src/main/java/com/group10/lazymanscooking/RecipesFragment.java
@@ -34,7 +34,7 @@ public class RecipesFragment extends Fragment {
         listView = (ListView) rootView.findViewById(R.id.recipesListView);
         recipes = new ArrayList<>();
         final ArrayAdapter<Recipe> arrayAdapter = new ArrayAdapter<Recipe>(getActivity(), android.R.layout.simple_list_item_1, recipes);
-        Bundle args = getArguments();
+        Bundle args = this.getArguments();
         if(args == null) {
             ParseQuery<ParseObject> query = ParseQuery.getQuery("Recipe");
             query.findInBackground(new FindCallback<ParseObject>() {
@@ -52,40 +52,63 @@ public class RecipesFragment extends Fragment {
             });
         }
         else{
-            ParseUser currentUser = ParseUser.getCurrentUser();
-            ParseQuery<ParseObject> favorites = ParseQuery.getQuery("Favorite");
-            favorites.whereEqualTo("user_id",currentUser.getObjectId() );
+            String search = args.getString("search", "");
+            Boolean favorite = args.getBoolean("favorite", false);
+            if (favorite) {
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                ParseQuery<ParseObject> favorites = ParseQuery.getQuery("Favorite");
+                favorites.whereEqualTo("user_id", currentUser.getObjectId());
 
 
-            ParseQuery<ParseObject> query = ParseQuery.getQuery("Recipe");
-            query.whereMatchesKeyInQuery("objectId","recipe_id",favorites);
-            query.findInBackground(new FindCallback<ParseObject>() {
-                public void done(List<ParseObject> objects, ParseException e) {
-                    if (e == null) {
-                        for (ParseObject recipe : objects) {
-                            Recipe mrecipe = new Recipe(recipe.getString("id"), recipe.getString("title"));
-                            recipes.add(mrecipe);
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("Recipe");
+                query.whereMatchesKeyInQuery("objectId", "recipe_id", favorites);
+                query.findInBackground(new FindCallback<ParseObject>() {
+                    public void done(List<ParseObject> objects, ParseException e) {
+                        if (e == null) {
+                            for (ParseObject recipe : objects) {
+                                Recipe mrecipe = new Recipe(recipe.getString("id"), recipe.getString("title"));
+                                recipes.add(mrecipe);
+                            }
+                            listView.setAdapter(arrayAdapter);
+                        } else {
+                            Toast.makeText(getActivity(), "Something went wrong while getting the recipes.", Toast.LENGTH_LONG).show();
                         }
-                        listView.setAdapter(arrayAdapter);
-                    } else {
-                        Toast.makeText(getActivity(), "Something went wrong while getting the recipes.", Toast.LENGTH_LONG).show();
                     }
-                }
-            });
+                });
+            }
+            else if (!search.isEmpty()) {
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("Recipe");
+                query.whereMatches("title", "("+search+")", "i");
+                query.findInBackground(new FindCallback<ParseObject>() {
+                    public void done(List<ParseObject> objects, ParseException e) {
+                        if (e == null) {
+                            for (ParseObject recipe : objects) {
+                                Recipe mrecipe = new Recipe(recipe.getString("id"), recipe.getString("title"));
+                                recipes.add(mrecipe);
+                            }
+                            listView.setAdapter(arrayAdapter);
+                        } else {
+                            Toast.makeText(getActivity(), "Something went wrong while getting the recipes.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }else {
+                //Do nothing
+            }
         }
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                public void onItemClick(AdapterView<?> arg0, View arg1, int position, long id) {
-                    //Change fragment
-                    RecipeFragment recipedetails = new RecipeFragment();
-                    Recipe passrecipe = (recipes.get(position));
-                    Bundle args = new Bundle();
-                    args.putSerializable("recipe",passrecipe);
-                    recipedetails.setArguments(args);
-                    FragmentTransaction ft = getFragmentManager().beginTransaction();
-                    ft.replace(R.id.pager, recipedetails);
-                    ft.show(recipedetails);
-                    ft.commit();
-                }
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long id) {
+                //Change fragment
+                RecipeFragment recipedetails = new RecipeFragment();
+                Recipe passrecipe = (recipes.get(position));
+                Bundle args = new Bundle();
+                args.putSerializable("recipe", passrecipe);
+                recipedetails.setArguments(args);
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.pager, recipedetails);
+                ft.show(recipedetails);
+                ft.commit();
+            }
         });
         return rootView;
     }
